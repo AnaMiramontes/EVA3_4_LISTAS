e3c667e41ca71446d3212df096ffdc217c02bd25
Ana <anamima1905@gmail.com>
2023-12-11 17:58:14 -0600

    a

---
+++ app/src/main/java/com/example/eva3_4_listas/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.eva3_4_listas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lstVwDatos;
+    String[] datos = {
+            "Tacos",
+            "Tacos de pastor",
+            "Tacos de alambre",
+            "Tacos de asada",
+            "Tacos de papa",
+            "Tacos de tripitas",
+            "Tacos de carnitas",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lstVwDatos = findViewById(R.id.lstDatos);
+        lstVwDatos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,datos));
+        lstVwDatos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Valor seleccionado "+ datos[i], Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, OtraPantalla.class);
+                intent.putExtra("DATO",datos[i]);
+                startActivity(intent);
+            }
+        });
+    }
+}
